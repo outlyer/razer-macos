aa52712ee814f47b48775d89c449c66dd69d46bb
Aubin Paul <outlyer@users.noreply.github.com>
2023-06-21 14:28:25 -0400

    Remove private information from script and use a dotenv file

--- release.sh
+++ release.sh
@@ -1,19 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Get signature information from a .env file with
+# APPLE_ID and APPLE_ID_PASSWORD in it
+#
+set -o allexport
+source .env
+set +o allexport
 
-export APPLE_ID=""
-export APPLE_ID_PASSWORD=""
-
+# Build
 yarn clean
 rm -rf ./node_modules ./dist
-
 yarn
 
+# Build Package
 yarn dist
 
-if [[ -z $APPLE_ID ]]
-then
+# Sign
+if [[ -z $APPLE_ID ]]; then
   codesign -s - --deep --force ./dist/mac-universal/Razer\ macOS.app
 fi
 
+# Clean Up
 unset APPLE_ID
 unset APPLE_ID_PASSWORD
